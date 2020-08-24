4ea306f259695ac74412dc658ad465eb4e680493
unknown <Administrator@sh-nongyanhua.SNDA.ROOT.CORP>
2020-08-24 19:28:21 +0800

    first commit

---
+++ Assets/Script/CameraFollow.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.AccessControl;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // 距离坦克的距离
+    public float distance = 10;
+    public float maxDistance = 20f;
+    public float minDistance = 5f;
+    public float zoomSpeed = 0.2f;
+    // 横向角度
+    public float rot = 0;
+    public float rotSpeed = 0.1f;
+
+    // 纵向角度
+    // 默认纵向角度为30度
+    public float roll = 30f * Mathf.PI * 2 / 360;
+    private float rollSpeed = 0.1f;
+    private float maxRoll = 60f * Mathf.PI * 2 / 360;
+    private float minRoll = 0;
+    private GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = GameObject.Find("tank");
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Debug.Log("LateUpdate");
+
+        if (target == null)
+            return;
+        if (Camera.main == null)
+            return;
+        Zoom();
+        Rotate();
+        Roll();
+        Vector3 targetPos = target.transform.position;
+        Vector3 cameraPos;
+        float d = distance * Mathf.Cos(roll);
+        float height = distance * Mathf.Sin(roll);
+        cameraPos.x = targetPos.x + d * Mathf.Cos(rot);
+        cameraPos.z = targetPos.z + d * Mathf.Sin(rot);
+        cameraPos.y = targetPos.y + height;
+
+        Camera.main.transform.position = cameraPos;
+        Camera.main.transform.LookAt(target.transform);
+    }
+
+    public void SetTarget(GameObject target)
+    {
+        if (target.transform.Find("cameraPoint") != null)
+        {
+            this.target = target.transform.Find("cameraPoint").gameObject;
+        }
+        else
+        {
+            this.target = target;
+        }
+    }
+    void Rotate()
+    {
+        float w = Input.GetAxis("Mouse X") * rotSpeed;
+        Debug.Log("Rotate: " + w.ToString());
+        rot -= w;
+    }
+    void Roll()
+    {
+        float w = Input.GetAxis("Mouse Y") * rollSpeed;
+        roll -= w;
+        if (roll > maxRoll)
+            roll = maxRoll;
+        if (roll < minRoll)
+            roll = minRoll;
+    }
+
+    void Zoom()
+    {
+        if(Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if (distance > minDistance)
+                distance -= zoomSpeed;
+
+        }else if(Input.GetAxis("Mouse ScrollWheel") < 0){
+            if (distance < maxDistance)
+                distance += zoomSpeed;
+        }
+    }
+
+}
---
+++ Assets/Script/Tank.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : MonoBehaviour
+{
+    // 移动速度
+    public float speed = 5f;
+    public float steer = 50;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        transform.Rotate(0, x * steer * Time.deltaTime, 0);
+
+        float y = Input.GetAxis("Vertical");
+        Vector3 s = y * transform.forward * speed * Time.deltaTime;
+        transform.transform.position += s;
+
+    }
+}
